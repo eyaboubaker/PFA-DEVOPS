6b6cb848343987e8e25a14231eb60c906cdcfba0
eyaboubaker <eyaboubaker0@gmail.com>
2022-05-25 00:06:19 +0200

    add the files

---
+++ src/main/java/com/esprit/clinique/model/Client.java
@@ -0,0 +1,63 @@
+package com.esprit.clinique.model;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+			
+	@Id		
+	@GenericGenerator(name = "increment", strategy = "increment")
+	@GeneratedValue(generator = "increment")
+	private long id;
+		
+	private String nom;			
+			
+	private String prenom;
+
+	public Client() {
+		super();
+	}
+
+	public Client(long id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+			
+}
+			
